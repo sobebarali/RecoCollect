d74b0a29aa7c17c861c5c7f9069caa801865ae27
sobebarali <sobebar.ali17@gmail.com>
2024-09-17 11:24:56 +0530

    Add validation test for missing collection name during creation

--- tests/collections/create/create.validation.test.ts
+++ tests/collections/create/create.validation.test.ts
@@ -3,6 +3,26 @@ import request from 'supertest';
 import { expect, describe, test } from '@jest/globals';
 
 describe('CREATE COLLECTIONS - VALIDATION', () => {
+  test('should fail to create a collections as name is missing', async () => {
+    const user_id = 1; 
+    const name = '';
+    const description = 'Best Action Movies';
+
+    const response = await request(app).post('/api/v1/collections').send({
+      user_id,
+      name,
+      description,
+    });
+
+    expect(response.status).toBe(400);
+    expect(response.body).toEqual({
+      data: null,
+      error: {
+        code: 'VALIDATION_ERROR',
+        message: '"name" is not allowed to be empty',
+      },
+    });
+  });
   test('should fail to create a collections as user_id is not a number', async () => {
     const user_id = '1'; //<--- user_id must be a number
     const name = 'Alice Collection';
